e13196129dc699b1f47efc5a77514178858bfe6e
unknown <mohsen.fatemi73@gmail.com>
2024-08-18 20:40:39 +0330

    Adding Folium Heatmap files

---
+++ Folium-Heatmap/Draw_map.py
@@ -0,0 +1,20 @@
+﻿import folium
+from folium.plugins import HeatMap
+from Get_and_Preprocess_data import *
+
+df = get_table(url='https://en.wikipedia.org/wiki/List_of_Iranian_four-thousanders')
+
+center_lat = df['Latitude'].mean()
+center_lon = df['Longitude'].mean()
+
+# first way
+map_base = folium.Map(location=[center_lat, center_lon], zoom_start=6)
+HeatMap(df[['Latitude', 'Longitude']]).add_to(map_base)
+map_base.save("heatmap.html")
+
+
+# second way
+map_base = folium.Map(location=[center_lat, center_lon], zoom_start=5)
+for idx, row in df.iterrows():
+    folium.Marker([row['Latitude'], row['Longitude']], popup=row['Persian name']).add_to(map_base)
+map_base.save("marker_map.html")
---
+++ Folium-Heatmap/Get_and_Preprocess_data.py
@@ -0,0 +1,31 @@
+﻿import re
+import pandas as pd
+
+def dms_to_decimal(dms_str):
+    # Regular expression to match the DMS format
+    pattern = r'([+-])(\d+)° (\d+)′?\'? (\d+\.?\d*)″?"?'
+    
+    # Extract components using regex
+    match = re.match(pattern, dms_str)
+    if not match:
+        raise ValueError("Invalid DMS format")
+    
+    sign, degrees, minutes, seconds = match.groups()
+    
+    # Convert to decimal degrees
+    decimal = int(degrees) + int(minutes)/60 + float(seconds)/3600
+    
+    # Apply sign
+    if sign == '-':
+        decimal = -decimal
+    
+    return round(decimal, 6)
+
+def get_table(url):
+    tables = pd.read_html(url)
+    df = tables[-1]
+    df.dropna(subset=['Latitude','Longitude'],inplace=True,how='all')
+    df['Latitude'] = df['Latitude'].apply(dms_to_decimal)
+    df['Longitude'] = df['Longitude'].apply(dms_to_decimal)
+    
+    return df
